b3bdefb67d16c35609e07522a210546e0746f62d
Tawfiqul Hasan <tawfiqul.abid@gmail.com>
2026-07-23 16:49:19 +0600

    Data dirven test using json file

---
+++ tests/test_parametrize_with_json.py
@@ -0,0 +1,23 @@
+import re
+import pytest
+from playwright.sync_api import expect
+from utils.json_read import load_json 
+
+
+login_data = load_json("test_data/login_data.json")
+
+@pytest.mark.parametrize("data", login_data)
+def test_parametrize_with_json(page, data):
+
+    username = data["username"]
+    password = data["password"]
+
+    page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+   
+    page.get_by_role("textbox", name="Username").fill(username)
+    
+    page.get_by_role("textbox", name="Password").fill(password)
+    
+    page.get_by_role("button", name="Login").click()
+    
+    expect(page.get_by_role("link", name="Dashboard")).to_be_visible()
